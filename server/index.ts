7e9ec261a4259f49eccb49500f18ca86238e23ee
abdul mukti <abdulmukti40390@gmail.com>
2025-12-17 17:11:52 +0700

    first commit

--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ app.use(
     verify: (req, _res, buf) => {
       req.rawBody = buf;
     },
-  }),
+  })
 );
 
 app.use(express.urlencoded({ extended: false }));
@@ -80,19 +80,17 @@ app.use((req, res, next) => {
     await setupVite(httpServer, app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
+  const host = process.env.HOST || "127.0.0.1";
+
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
+      host: host,
       reusePort: true,
     },
     () => {
       log(`serving on port ${port}`);
-    },
+    }
   );
 })();
