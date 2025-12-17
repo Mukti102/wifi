a848a7f5c5073ad563d3906da3b389ed80b8f95b
abdul mukti <abdulmukti40390@gmail.com>
2025-12-17 17:20:17 +0700

    commit

--- server/index.ts
+++ server/index.ts
@@ -83,14 +83,20 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || "5000", 10);
   const host = process.env.HOST || "127.0.0.1";
 
-  httpServer.listen(
-    {
-      port,
-      host: host,
-      reusePort: true,
-    },
-    () => {
-      log(`serving on port ${port}`);
-    }
-  );
+  // httpServer.listen(
+  //   {
+  //     port,
+  //     host: host,
+  //     reusePort: true,
+  //   },
+  //   () => {
+  //     log(`serving on port ${port}`);
+  //   }
+  // );
+
+
+httpServer.listen(port, () => {
+  log(`serving on http://localhost:${port}`);
+});
+
 })();
